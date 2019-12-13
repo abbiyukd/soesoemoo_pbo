d59a08fa69e50efebac188f01f278a03fdefe1f9
abbiyukd <38352511+abbiyukd@users.noreply.github.com>
2019-12-13 22:22:23 +0700

    Add files via upload

---
+++ src/soesoemoo_pbo/SoesoeMoo_PBO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soesoemoo_pbo;
+
+/**
+ *
+ * @author Admin
+ */
+public class SoesoeMoo_PBO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frontframe fr1 = new frontframe();
+        fr1.setVisible(true);
+    }
+    
+}
